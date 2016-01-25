624e3261983afd801ad7a0a3baaeef51b27b7072
fred chang <fchang@fas.harvard.edu>
2016-01-25 15:20:43 -0500

    code to parse parameters, either structures or name value pairs

--- fcAnalysis/fcHelper/updateParams.m
+++ fcAnalysis/fcHelper/updateParams.m
@@ -4,9 +4,9 @@ function params = updateParams(params,userParams)
 
 if ~isempty(userParams)
     if isstruct(userParams)
-        
+        params = setstructfields(params,userParams);
     else
-        
+        params = parsepropval(params,userParams{:});
     end
 end
 
