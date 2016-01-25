a7c2b93cad7c98865aaaabfeba0370303acd4af1
fred chang <fchang@fas.harvard.edu>
2016-01-25 14:32:31 -0500

    added function that parses parameters and over-rides default ones

---
+++ fcAnalysis/fcHelper/updateParams.m
@@ -0,0 +1,14 @@
+function params = updateParams(params,userParams)
+%UPDATEPARAMS takes userParams, which can be a structure or a named,value
+%cell list, and updates the params structure fields.
+
+if ~isempty(userParams)
+    if isstruct(userParams)
+        
+    else
+        
+    end
+end
+
+end
+
