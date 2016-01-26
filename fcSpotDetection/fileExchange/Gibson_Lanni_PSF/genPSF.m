34b0276f788213780f8b433224514dd36be14e55
fred chang <fchang@fas.harvard.edu>
2016-01-26 10:57:45 -0500

    almost there

--- fcSpotDetection/fileExchange/Gibson_Lanni_PSF/genPSF.m
+++ fcSpotDetection/fileExchange/Gibson_Lanni_PSF/genPSF.m
@@ -21,7 +21,7 @@ params.lambda       = 5.8100e-07;
 params.M            = 60;
 params.NA           = 1.4500;
 params.alpha        = asin(params.NA/params.Ni);
-params.pixelSize    = 16e-06/4;
+params.pixelSize    = 6.5e-06;
 params.f            = 5;
 params.mode         = 1;
 % default position of the PSF
@@ -31,7 +31,7 @@ params.zp           = 0;
 % calculating ru
 FWHM = 0.353/params.NA;
 sigma = FWHM/2.355;
-params.ru           = round(4*sigma);
+params.ru           = 20;
 % default zstep parameters
 params.dz           = 0.25e-6;
 params.zSteps       = 25;
@@ -42,7 +42,12 @@ params = updateParams(params,varargin);
 %% generate the zSteps centered at the PSF 
 bounds = params.dz*(params.zSteps - 1)/2;
 z = linspace(params.z0 - bounds, params.z0 + bounds, params.zSteps);
-psfData = vectorialPSF(params.xp, params.yp, params.zp, z, params.ru, params);
+% this is the parameter structure that vectorialPSF needs, values are not
+% important.
+p=struct('Ti0',1.3000e-04,'Ni0', 1.5180,'Ni',1.5180, 'Tg0', 1.7000e-04,...
+    'Tg', 1.7000e-04, 'Ng0', 1.5150, 'Ng', 1.5150, 'Ns' , 1.33,...
+    'lambda',5.5000e-07, 'M' , 100, 'NA' , 1.400, 'alpha', 0, 'pixelSize', 10.0e-06);
+psfData = vectorialPSF(params.xp, params.yp, params.zp, z, params.ru, curateStruct(p,params));
 
 %% generate nd PSF
 if ~isempty(thresh)
