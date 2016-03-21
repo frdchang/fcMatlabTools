c79e663d4d1a71f6e69e744f085e1dd3912811d9
fred chang <fchang@fas.harvard.edu>
2016-03-21 13:52:59 -0400

    removed old code since new one works
    Former-commit-id: 2ec71a79d05765bd8ed8c07781091ce2f8ad2c40 [formerly eee31686cf149b2d75c3adb475dc3d3115f3f4d2]
    Former-commit-id: a39a0e909f80d657d2e2dd837882d53451a1db89

--- fcSpotDetection/helper/findSpotStages/archive/DLogDLamba_PP.m
+++
@@ -1,9 +0,0 @@
-function DLogDLamba = DLogDLamba_PP(d,lambda,sigmasq)
-%DLOGDLAMPP calculates the deriviative of the log likelihood w.r.t.
-% lambda given d = data, lambda = theoretical value, and sigmasq = variance
-% of the pixel read noise.  
-% the log likelihood function is in Poisson * Poisson form (PP)
-
-DLogDLamba = (d - lambda) ./ (bsxfun(@plus,lambda,sigmasq));
-end
-
