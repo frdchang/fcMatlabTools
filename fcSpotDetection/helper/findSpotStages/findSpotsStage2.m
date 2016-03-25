91654574b53eeefbfe661fe89d97eefa7e01c2a3
fred chang <fchang@fas.harvard.edu>
2016-03-25 09:01:15 -0400

    skeleton code of stage 2

--- fcSpotDetection/helper/findSpotStages/findSpotsStage2.m
+++ fcSpotDetection/helper/findSpotStages/findSpotsStage2.m
@@ -1,7 +1,20 @@
-function [ output_args ] = findSpotsStageOneProcessing( input_args )
-%FINDSPOTSSTAGEONEPROCESSING Summary of this function goes here
-%   Detailed explanation goes here
+function candidates = findSpotsStage2(detected,varargin)
+%FINDSPOTSSTAGE2 Summary of this function goes here
+% 
+% detected:     output struct from findSpotsStage1
+% 
+% [notes] - a heads up for the user
+% 
+% [param cascade] -> subFunc1
+%                 -> subFunc2
 
+%--parameters--------------------------------------------------------------
+params.LLratioLocalPeak  = 15;
+params.minVol            = 20;
+%--------------------------------------------------------------------------
+params = updateParams(params,varargin);
+
+BWmask = bwareaopen(BWmask, minVolume,6);
 
 end
 
