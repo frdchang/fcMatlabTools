bbcecac4695caba3f6f7987328f7f074e3bec9ba
fred chang <fchang@fas.harvard.edu>
2016-03-30 10:10:55 -0400

    stage 2 is simply threshold of LLRatio and feathering
    Former-commit-id: cabe8f8e032a6b7b3781b32efc243640c3760d8a [formerly 2a6639d5969d25667d3c1ef3d0a2764c4ed4d885]
    Former-commit-id: 426e681a04425dd5b3f6c7616d47c90605b382d2

--- fcSpotDetection/helper/findSpotStages/findSpotsStage2.m
+++ fcSpotDetection/helper/findSpotStages/findSpotsStage2.m
@@ -1,17 +1,20 @@
 function candidates = findSpotsStage2(detected,varargin)
 %FINDSPOTSSTAGE2 find candidate regions among the processed output of the
-% data from findSpotsStage1 and applies iterative MLE, findSpotStage3, upon
-% the candidates.
+% data from findSpotsStage1 by thresholding the Log Likelihood Ratio and
+% feathering that output.  
 % 
 % detected:     output struct from findSpotsStage1
-% candidates:   output data structure organized as...
+% candidates:   output data structure organized as ...
+%
+% [notes] - this function output candidates as connected components that
+%           does not have to be rectangular bounding boxes.  this
+%           accomodates the fitting of spots to complex shapes.
 
 
 %--parameters--------------------------------------------------------------
 params.LLRatioLocalPeak  = 15;
 params.minVol            = 15;
-params.kernSize          = [7 7 7];
-params.featherSize       = [5 5 5];
+params.featherSize       = [3 3 3];
 %--------------------------------------------------------------------------
 params = updateParams(params,varargin);
 
@@ -21,39 +24,44 @@ peaks = detected.LLRatio.*(detected.LLRatio>params.LLRatioLocalPeak);
 % find non-zero peaks with a minum volume of pixels.  this number is the
 % autocorrelation size and depends on the SNR
 BWmask = bwareaopen(peaks>0, params.minVol,6);
-
-
-%% fulfill minimum criteria for kernSize
-% find centroids of each connected region and put a kernSize region there
-
-%% fulfill minimum criteria for skeleton
-% remove small regions that are bigger than kernSize in a given dimension
-% then dilate from there
-
-%% do the union of the above operations
-
-
-
-
-
-
-% expand the volumes by feathersize.  
-featherSizeStrel = strel('arbitrary',ones(params.featherSize));
-BWmask = imdilate(BWmask,featherSizeStrel);
-
-% find kernel size regions
-kernSizeStrel = strel('arbitrary',ones(params.kernSize));
-BWmask = imopen(BWmask,kernSizeStrel);
-
-
-peaks = peaks.*BWmask;
+plot3Dstack(BWmask);
+% feather accepted peaks
+BWmask = imdilate(BWmask,strel('arbitrary',ones(params.featherSize)));
+plot3Dstack(BWmask);
 stats = regionprops(BWmask,'PixelIdxList','PixelList');
 
 candidates.peaks = peaks;
 candidates.stats = stats;
 
-% find maximum values in each candidates as the initial position, amp and
-% background then launch findSpotsStage3 on them
+
+% %% fulfill minimum criteria for kernSize
+% % find centroids of each connected region and put a kernSize region there
+% 
+% %% fulfill minimum criteria for skeleton
+% % remove small regions that are bigger than kernSize in a given dimension
+% % then dilate from there
+% 
+% %% do the union of the above operations
+% 
+% 
+% 
+% 
+% 
+% 
+% % expand the volumes by feathersize.  
+% featherSizeStrel = strel('arbitrary',ones(params.featherSize));
+% BWmask = imdilate(BWmask,featherSizeStrel);
+% 
+% % find kernel size regions
+% kernSizeStrel = strel('arbitrary',ones(params.kernSize));
+% BWmask = imopen(BWmask,kernSizeStrel);
+% 
+% 
+% peaks = peaks.*BWmask;
+
+% 
+% % find maximum values in each candidates as the initial position, amp and
+% % background then launch findSpotsStage3 on them
 
 
 
