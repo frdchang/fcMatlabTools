c67e5e3486be7794d3265fab0756198e37f080bf
fred chang <fchang@fas.harvard.edu>
2016-04-01 10:54:34 -0400

    need to think about what is stage 3
    Former-commit-id: 617f0c7aa441128160fd231010e0ff770492d147 [formerly ac92946c39c8fed2d7c308d3340ed574e3305c6f]
    Former-commit-id: c2332c49c546e87caa6c69aa9ffa0d854d22cc89

--- fcSpotDetection/helper/findSpotStages/findSpotsStage3.m
+++ fcSpotDetection/helper/findSpotStages/findSpotsStage3.m
@@ -1,6 +1,12 @@
-function spotParamStruct = findSpotsStage3(data,varargin)
+function spotParamStruct = findSpotsStage3(data,givenStat,numSpotsToFit,varargin)
 %FINDSPOTSSTAGETHREE Summary of this function goes here
-%   Detailed explanation goes here
+% 
+% arg1:     arg1 is this
+% arg2:     arg2 is that
+% 
+% [notes] - a heads up for the user
+% 
+% [param cascade] -> MLEbyIteration
 
 %--parameters--------------------------------------------------------------
 params.stepSize     = 1;
@@ -8,4 +14,4 @@ params.stepSize     = 1;
 params = updateParams(params,varargin);
 
 
-
+state = MLEbyIteration(data,theta0,readNoise,domains,type,params{:});
