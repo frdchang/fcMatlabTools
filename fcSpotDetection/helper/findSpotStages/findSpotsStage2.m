a9a705fcf89fa3fd2f316a294d692b8494c2da7c
fred chang <fchang@fas.harvard.edu>
2016-06-03 09:32:20 -0400

    adding smoothing by the autocorrelation of the pattern

--- fcSpotDetection/helper/findSpotStages/findSpotsStage2.m
+++ fcSpotDetection/helper/findSpotStages/findSpotsStage2.m
@@ -1,4 +1,4 @@
-function candidates = findSpotsStage2(detected,varargin)
+function candidates = findSpotsStage2(detected,spotData,varargin)
 %FINDSPOTSSTAGE2 find candidate regions among the processed output of the
 % data from findSpotsStage1 by thresholding the Log Likelihood Ratio and
 % feathering that output.  
@@ -9,19 +9,29 @@ function candidates = findSpotsStage2(detected,varargin)
 % [notes] - this function output candidates as connected components that
 %           does not have to be rectangular bounding boxes.  this
 %           accomodates the fitting of spots to complex shapes.
+%         - this function filters the LLRATIO > thresh and A parameter > 0,
+%           then smooths it to do hdome local maxima search.  
+%         - then for those connected objects smaller than some value gives
+%         
 %
 % [param cascade] -> simpleThresholdDetection
 
 %--parameters--------------------------------------------------------------
 params.LLRatioThresh     = 50;
 params.minVol            = 3;
-params.featherSize       = [3 3 3];
+params.smoothingKernel   = [0.9,0.9,0.9];
+params.smoothingSize     = [7 7 7];
+% params.featherSize       = [3 3 3];
 %--------------------------------------------------------------------------
 params = updateParams(params,varargin);
 
+% smooth LLRatio
+[~,sepKernel] = ndGauss(params.smoothingKernel,params.smoothingSize);
+smoothLLRatio = convSeparableND(detected.LLRatio,sepKernel);
+
 % threshold by the value selected above
 % only select areas with A1 > 0
-peaks = detected.LLRatio.*(detected.LLRatio>params.LLRatioThresh).*(detected.A1>0);
+peaks = smoothLLRatio.*(detected.LLRatio>params.LLRatioThresh).*(detected.A1>0);
 BWmask = peaks>0;
 
 % find non-zero peaks with a minum volume of pixels.  this number is the
@@ -31,8 +41,8 @@ BWmask = bwareaopen(BWmask, params.minVol,6);
 % fill holes
 BWmask = imfill(BWmask,'holes');
 
-% feather
-BWmask = imdilate(BWmask,strel('arbitrary',ones(params.featherSize)));
+% % feather
+% BWmask = imdilate(BWmask,strel('arbitrary',ones(params.featherSize)));
 
 % segment and output
 stats = regionprops(BWmask,'PixelIdxList','PixelList');
