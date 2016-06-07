89bcf4b3e3d9606ed48e04045bed1e47eb9c3a38
fred chang <fchang@fas.harvard.edu>
2016-06-07 10:42:02 -0400

    qpm is not working againg

--- fcAnalysis/fcImageTools/QuantitativePhaseMapping/genQPM.m
+++ fcAnalysis/fcImageTools/QuantitativePhaseMapping/genQPM.m
@@ -1,18 +1,18 @@
 function [qpm] = genQPM(stack)
-startExp = 4;
-        % these parameters seem good for yeast
-        Nsl         = 50;%50;
-        lambda      = 514e-9;
-        ps          = 6.5e-6 / 60;%108.33e-9;%109e-9;%3.12e-7;
-        dz          = 300e-9;
-  
-        eps1 = 0.1;
-        eps2 = 0.1;
-        reflect = 0;
+
+% these parameters seem good for yeast
+Nsl         = 50;
+lambda      = 514e-9;
+ps          = 6.5e-6 / 60;
+dz          = 300e-9;
+
+eps1 = 0.1;
+eps2 = 0.1;
+reflect = 0;
 [~,~,zL] = size(stack);
 
-    nFocus = findBestFocus(stack);
-    display(['getQPM():best focus found at slice ' num2str(nFocus)]);
+nFocus = findBestFocus(stack);
+display(['getQPM():best focus found at slice ' num2str(nFocus)]);
 
 
 
@@ -20,34 +20,13 @@ zSteps = 1:zL;
 zSteps = zSteps - nFocus;
 zSteps = zSteps * dz;
 
-
-% subsample using exponential spacing so to cut down on computation time
-% for the qpm
-% if true
-%     display('getQPMfromPhase():using exponential sampling');
-%     % always take 10 around zFocus
-%     upperHalf = nFocus:zL;
-%     lowerHalf = fliplr(1:nFocus);
-%     % after the 10 around zFocus start skipping 1,then 2, then 3, then to
-%     % the end
-%     if numel(upperHalf) > startExp
-%         expSpacing = genExpSpacing(startExp,numel(upperHalf));
-%         upperHalfExp = upperHalf(expSpacing);
-%     end
-%     % do the lower end
-%     if numel(lowerHalf) > startExp
-%         expSpacing = genExpSpacing(startExp,numel(lowerHalf));
-%         lowerHalfExp = lowerHalf(expSpacing);
-%     end
-%     sampleExp = [fliplr(lowerHalfExp) upperHalfExp(2:end)];
-%     nFocus = find(sampleExp == nFocus);
-%     zSteps = zSteps(sampleExp);
-%     stack = stack(:,:,sampleExp);
-% end
-% RePhase=RunGaussionProcess(Ividmeas,zfocus,z,lambda,ps,Nsl,eps1,eps2,reflect)
 tic;
 qpm = RunGaussionProcess(double(stack),nFocus,zSteps',lambda,ps,Nsl,eps1,eps2,reflect);
 toc
+
+se = strel('ball',100,100);
+correctedQPM = imtophat(qpm,se);
+% need to figure out rolling ball subtraction for matlba
 % switch doBgkndSub
 %     case 'rollingBall'
 %         qpm = imtophat(qpm,strel('ball',diskR,diskR));
