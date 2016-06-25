2ca72a3eed1bf508a4253852d350e4cc15abd2c0
Frederick Chang <fchang@fas.harvard.edu>
2016-06-25 08:50:18 -0400

    update params now can do named value pairs and a struct at the same time

--- fcAnalysis/fcHelper/updateParams.m
+++ fcAnalysis/fcHelper/updateParams.m
@@ -17,21 +17,47 @@ function params = updateParams(params,userParams)
 %    end
 % end
 
-if ~isempty(userParams)
-   if iscell(userParams)
-       if isstruct(userParams{1})
-           params = setstructfields(params,userParams{1});
-       else
-           params = parsepropvalFC(params,userParams{:});
-       end   
-   else
-       if isstruct(userParams)
-           params = setstructfields(params,userParams);
-       else
-           error('userParams is not the type expected');
-       end
-   end
+if isempty(userParams)
+   return; 
 end
+
+if iscell(userParams)
+    % check if there is a param struct 
+    findTheStructs = find(cellfun(@isstruct,userParams)==1);
+    % make sure that it is in an odd position so that it is not a
+    % named-value pair
+    findTheParamStructs = find(isodd(findTheStructs));
+    findTheParamStructs = findTheStructs(findTheParamStructs);
+    % update params
+    for i = 1:numel(findTheParamStructs)
+       params = setstructfields(params,userParams{findTheParamStructs(i)}); 
+    end
+    % delete the param structs
+    userParams(findTheParamStructs) = [];
+    % update the named value pairs
+    params = parsepropvalFC(params,userParams{:});
+    
+elseif isstruct(userParams)
+    params = setstructfields(params,userParams);
+else
+   error('userParam argument neither a cell of named-value pairs or a struct'); 
+end
+
+% if ~isempty(userParams)
+%    if iscell(userParams)
+%        if isstruct(userParams{1})
+%            params = setstructfields(params,userParams{1});
+%        else
+%            params = parsepropvalFC(params,userParams{:});
+%        end   
+%    else
+%        if isstruct(userParams)
+%            params = setstructfields(params,userParams);
+%        else
+%            error('userParams is not the type expected');
+%        end
+%    end
+% end
 
 end
 
---
+++ fcAnalysis/fcHelper/variable2string.m
@@ -0,0 +1,5 @@
+function out = variable2string(var)
+%VARIABLE2STRING returns the string of the variable name
+  out = inputname(1);
+end
+
