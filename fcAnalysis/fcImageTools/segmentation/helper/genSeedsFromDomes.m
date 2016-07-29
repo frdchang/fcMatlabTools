33f6884a95539ccc041aa045f788a4359a42c1f5
fred chang <fchang@fas.harvard.edu>
2016-07-29 17:25:37 -0400

    fixed when data is 1 pixel

--- fcAnalysis/fcImageTools/segmentation/helper/genSeedsFromDomes.m
+++ fcAnalysis/fcImageTools/segmentation/helper/genSeedsFromDomes.m
@@ -1,13 +1,13 @@
 function [seeds,otsuSubsetBW] = genSeedsFromDomes(img,varargin)
-% GENSEEDSFROMDOMES 
+% GENSEEDSFROMDOMES
 % arg1:     arg1 is this
 % arg2:     arg2 is that
-% 
+%
 % [notes] - a heads up for the user
-% 
+%
 % [param cascade] -> subFunc1
 %                 -> subFunc2
- 
+
 %--parameters--------------------------------------------------------------
 params.domeH            = 10;
 params.rdisk            = 5;
@@ -18,6 +18,7 @@ params = updateParams(params,varargin);
 
 % generate gaussian for smoothing
 gauss = fspecial('gaussian',round(params.gaussianSigma*5),params.gaussianSigma);
+gauss = gauss / sum(gauss(:));
 gauss = threshPSF(gauss,0.0015);
 gauss = gauss / sum(gauss(:));
 imgGauss   = imfilter(img,gauss);
@@ -38,13 +39,15 @@ seeds = zeros(size(img));
 for ii = 1:numel(stats)
     subDome = getSubsetwBBoxND(hdomed,stats(ii).BoundingBox);
     subDome = subDome / sum(subDome(:));
-    sampledDome = representativeSampling(subDome);
-    [clustcent,~,~] = MeanShiftCluster(sampledDome,params.meanShiftSize);
-    for jj = 1:size(clustcent,2)
-        currClust = clustcent(:,jj);
-        clustOrig = BBox2OrigCoor(stats(ii).BoundingBox,currClust);
-        clustOrig = num2cell(round(clustOrig'));
-        seeds(clustOrig{:}) = 1;
+    if numel(subDome)>1
+        sampledDome = representativeSampling(subDome);
+        [clustcent,~,~] = MeanShiftCluster(sampledDome,params.meanShiftSize);
+        for jj = 1:size(clustcent,2)
+            currClust = clustcent(:,jj);
+            clustOrig = BBox2OrigCoor(stats(ii).BoundingBox,currClust);
+            clustOrig = num2cell(round(clustOrig'));
+            seeds(clustOrig{:}) = 1;
+        end
     end
 end
 
--- fcAnalysis/fcImageTools/segmentation/segRecipes.m
+++ fcAnalysis/fcImageTools/segmentation/segRecipes.m
@@ -1,5 +1,5 @@
 %% for cyano segmentation
-brightZstack = importStack('/Users/fchang/Dropbox/Public/examplesFromAndrian/ex3cult-IP-Channel Alignment-04.czi - T=0 C=0.tif');
+brightZstack = importStack('/Users/fchang/Dropbox/Public/examplesFromAndrian/AGH42-od003_Alignment_Subset.czi - T=0 C=2.tif');
 % when you run cyano seg it will ask you to 
 % 1) select a zoomed area to closer inspection
 % 2) select a handful of points that define the edge (the bright halo
