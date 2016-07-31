4c0dfa113660529a76c3589377b60226cec02689
fred chang <fchang@fas.harvard.edu>
2016-07-31 08:47:02 -0400

    added a way from spot params to be extractec from L

--- fcAnalysis/fcImageTools/segmentation/segRecipes.m
+++ fcAnalysis/fcImageTools/segmentation/segRecipes.m
@@ -1,5 +1,6 @@
 %% for cyano segmentation
-brightZstack = importStack('/Users/fchang/Dropbox/Public/examplesFromAndrian/AGH42-od003_Alignment_Subset.czi - T=0 C=2.tif');
+brightZstack = importStack('/Users/fchang/Dropbox/Public/examplesFromAndrian/AGH42example.ome.tiff - T=0 C=2.tif');
+spotStack    = importStack('/Users/fchang/Dropbox/Public/examplesFromAndrian/AGH42example.ome.tiff - T=0 C=0.tif');
 % when you run cyano seg it will ask you to 
 % 1) select a zoomed area to closer inspection
 % 2) select a handful of points that define the edge (the bright halo
@@ -7,3 +8,9 @@ brightZstack = importStack('/Users/fchang/Dropbox/Public/examplesFromAndrian/AGH
 [L,stats,qpm] = cyanoSeg(brightZstack);
 [~,rgbSegmented]= plotLabels(qpm,stats,L);
 figure;imshow(rgbSegmented);
+
+spotParams = fcSpotDetection(spotStack);
+plot3Dstack(spotStack,'spotParams',spotParams);
+
+% this function will return spotparams for each cell in L
+spotParamsInL = returnSpotParamsInL(spotParams,L);
