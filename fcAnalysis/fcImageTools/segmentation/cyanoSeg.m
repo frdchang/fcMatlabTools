ee0554a7d4c17f27fabb1a4e3216eab05111ee11
fred chang <fchang@fas.harvard.edu>
2016-08-19 23:00:58 -0400

    updated cyano stage

--- fcAnalysis/fcImageTools/segmentation/cyanoSeg.m
+++ fcAnalysis/fcImageTools/segmentation/cyanoSeg.m
@@ -1,28 +1,41 @@
-function [L,stats,qpm] = cyanoSeg(brightZstack)
+function [L,stats,qpm] = cyanoSeg(brightZstack,varargin)
 %CYANOSEG Summary of this function goes here
 %   Detailed explanation goes here
 % edgeZProfile = [16962 17033 17319 17392 18426 21676 23678 24500 24320]
 
+
+%--parameters--------------------------------------------------------------
+params.edgeProfileZ     = [];
+params.minArea          = 200;
+%--------------------------------------------------------------------------
+params = updateParams(params,varargin);
+
+
 brightZstack = double(brightZstack);
 
-[x,y] = getline_zoom(maxintensityproj(brightZstack,3));
-% average all the points to get one edgeProfile
-edgeProfileZ = zeros(numel(x),size(brightZstack,3));
-for i = 1:numel(x);
-    currProfile = brightZstack(round(y(i)),round(x(i)),:);
-    edgeProfileZ(i,:) = currProfile(:);
+if isempty(params.edgeProfileZ)
+    [x,y] = getline_zoom(maxintensityproj(brightZstack,3));
+    % average all the points to get one edgeProfile
+    edgeProfileZ = zeros(numel(x),size(brightZstack,3));
+    for i = 1:numel(x);
+        currProfile = brightZstack(round(y(i)),round(x(i)),:);
+        edgeProfileZ(i,:) = currProfile(:);
+    end
+    params.edgeProfileZ = mean(edgeProfileZ);
+    
+    display(['''edgeProfileZ'', ' mat2str(edgeProfileZ)]);
 end
-edgeProfileZ = mean(edgeProfileZ);
-figure;plot(edgeProfileZ);
-qpm = genQPM(brightZstack,'ballSize',20);
 
+qpm = genQPM(brightZstack,'ballSize',20);
 [seeds,foregroundMask] = genSeedsFromDomes(qpm);
-edgeMask = genEdgeMapFromZ(brightZstack,edgeProfileZ);
+foregroundMask = bwareaopen(foregroundMask,200);
+edgeMask = genEdgeMapFromZ(brightZstack,params.edgeProfileZ);
 cells = -edgeMask;
 threshCells = multithresh(cells(:));
 cells = cells > threshCells;
 cells =imreconstruct(foregroundMask,cells,4);
 cells = imfill(cells,'holes');
+cells = imclearborder(cells);
 CC = bwconncomp(cells,4);
 stats = regionprops(CC);
 L  = labelmatrix(CC);
