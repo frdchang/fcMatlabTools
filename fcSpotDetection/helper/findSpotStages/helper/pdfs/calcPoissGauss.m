f4a9c7b9ac2ed79ef33de328f6fec479a9e317e4
fred chang <fchang@fas.harvard.edu>
2016-09-11 08:57:38 -0400

    poisson gaussian noise is being built

--- fcSpotDetection/helper/findSpotStages/helper/pdfs/calcPoissGauss.m
+++ fcSpotDetection/helper/findSpotStages/helper/pdfs/calcPoissGauss.m
@@ -1,15 +1,19 @@
 function [probData] = calcPoissGauss(data,lambda,sigma)
 %POISSGAUSS returns the probability of seeing data given lambda sigma and
-%zero offset, for now it is not vectorized so provide one at a time.
+%zero offset, for now it is not vectorized.
 
+probData = arrayfun(@localCalcPoissGauss,data,lambda,sigma);
+
+end
+
+function probData = localCalcPoissGauss(data,lambda,sigma)
 gaussDomSize = 4;
 
 sigmaSteps = round(gaussDomSize*sigma);
 gaussDOM = -sigmaSteps:sigmaSteps;
 gauss = normpdf(gaussDOM,0,sigma);
 gauss = gauss/sum(gauss(:));
-poiss = poisspdf(data+gaussDOM,lambda);
+poiss = contPoissPDF(data+gaussDOM,lambda);
 probData = sum(gauss.*poiss);
 
 end
-
