51ffb9212c6e841f66ed0ef3eaa28f42312d6e78
Frederick Chang <fchang@fas.harvard.edu>
2016-10-07 14:42:39 -0400

    added some references so my linux workstation can run

--- fcAnalysis/fcProcessing/processRecipes.m
+++ fcAnalysis/fcProcessing/processRecipes.m
@@ -1,11 +1,11 @@
 %% processing data
 phaseRegexp = 'BrightFieldTTL';
 spotRegexp = {'FITC\(WhiteTTL\)'};
-expFolder = '/Users/fchang/Dropbox/Public/smalldataset/fcData/201601-test-adf';
-
+% expFolder = '/Users/fchang/Dropbox/Public/smalldataset/fcData/201601-test-adf';
+expFolder = '/mnt/btrfs/fcDataStorage/fcNikon/fcData/20160915-mitosis-BWY804_4-4/doTimeLapse_1';
 phaseFiles = getAllFiles(expFolder,phaseRegexp);
 spotFiles = getAllFiles(expFolder,spotRegexp);
 
-test = applyFuncTo_ListOfFiles(phaseFiles,@openImage_applyFuncTo,{},@genQPM,{},@saveToProcessed_images,{});
+test = applyFuncTo_ListOfFiles(phaseFiles,@openImage_applyFuncTo,{},@genQPM,{},@saveToProcessed_images,{},'doParallel',true);
 
 
