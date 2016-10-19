f30e30ee6bb322610ed68f0b347b94471a5db9ce
fred chang <fchang@fas.harvard.edu>
2016-10-18 20:56:01 -0400

    can handle nd data

--- fcAnalysis/fcImageTools/segmentation/segmentationPlotting/plotBBox.m
+++ fcAnalysis/fcImageTools/segmentation/segmentationPlotting/plotBBox.m
@@ -2,7 +2,7 @@ function h = plotBBox(data,BBox)
 %PLOTBBOX takes data and plots BBox
 
 figure;
-imshow(data);
+imshow(xyMaxProjND(data));
 hold on;
 rectangle('Position', BBox,...
 	'EdgeColor','r', 'LineWidth', 3);
