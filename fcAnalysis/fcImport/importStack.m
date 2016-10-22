e13ea03525a408b0d2cfdf99638eb7a15c0aaed2
fred chang <fchang@fas.harvard.edu>
2016-10-22 08:05:35 -0400

    when you give it a filename that does not exist it returns empty

--- fcAnalysis/fcImport/importStack.m
+++ fcAnalysis/fcImport/importStack.m
@@ -2,7 +2,10 @@ function stack = importStack(filename)
 %IMPORTSTACK imports either tif or fits stack.
 
 [~,~,ext] = fileparts(filename);
-
+if exists(filename) == 0
+   stack = [];
+   return;
+end
 switch ext
     case {'.fit','.fits'}
         stack = importFits(filename);
