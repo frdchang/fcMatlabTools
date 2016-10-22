8e44ead872de92806c1df5b3df99d7e0dd28f470
fred chang <fchang@fas.harvard.edu>
2016-10-22 08:09:26 -0400

    handles the empty or non existent filename and returns an empty

--- fcAnalysis/fcImport/loadAndTakeFirstField.m
+++ fcAnalysis/fcImport/loadAndTakeFirstField.m
@@ -2,6 +2,10 @@ function firstField = loadAndTakeFirstField(filepath)
 %LOADANDTAKEFIRSTFIELD Summary of this function goes here
 %   Detailed explanation goes here
 
+if exist(filepath) == 0
+   firstField = [];
+   return;
+end
 firstField = load(filepath);
 daFields = fieldnames(firstField);
 firstField = firstField.(daFields{1});
