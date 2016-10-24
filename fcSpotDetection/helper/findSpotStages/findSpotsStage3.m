aea7fbb5eebe5cb8f4538386667fda3e4b5ad041
fred chang <fchang@fas.harvard.edu>
2016-10-24 16:47:41 -0400

    made it more memory efficient for parfor

--- fcSpotDetection/helper/findSpotStages/findSpotsStage3.m
+++ fcSpotDetection/helper/findSpotStages/findSpotsStage3.m
@@ -20,25 +20,44 @@ params = updateParams(params,varargin);
 numCandidates = numel(candidates.stats);
 spotParams = cell(numCandidates,1);
 
+currStatBasket = cell(numCandidates,1);
+currDataBasket = cell(numCandidates,1);
+currNoiseBasket = cell(numCandidates,1);
+currDomainBasket = cell(numCandidates,1);
+currA1sBasket = cell(numCandidates,1);
+currB1sBasket = cell(numCandidates,1);
+currB0sBasket = cell(numCandidates,1);
+currLLRBasket = cell(numCandidates,1);
+for i = 1:numCandidates
+    currStats           = candidates.stats(i);
+    currStatBasket{i}   = currStats;
+    currDataBasket{i}   = data(currStats.PixelIdxList);
+    currNoiseBasket{i}  = readNoise(currStats.PixelIdxList);
+    currDomainBasket{i} = num2cell(currStats.PixelList,1)';
+    currA1sBasket{i}    = detected.A1(currStats.PixelIdxList);
+    currB1sBasket{i}    = detected.B1(currStats.PixelIdxList);
+    currB0sBasket{i}    = detected.B0(currStats.PixelIdxList);
+    currLLRBasket{i}    = detected.LLRatio(currStats.PixelIdxList);
+end
+
 setupParForProgress(numCandidates);
 for i = 1:numCandidates
-    currStats   = candidates.stats(i);
     % extract relevant data and its domain
-    currData        = data(currStats.PixelIdxList);
-    currReadNoise   = readNoise(currStats.PixelIdxList);
-    currDomains     = num2cell(currStats.PixelList,1)';
+    currData        = currDataBasket{i};
+    currReadNoise   = currNoiseBasket{i};
+    currDomains     = currDomainBasket{i};
     % define theta0 among subset that is A1 > 0
-    posA1s              = detected.A1(currStats.PixelIdxList)>0;
-    if sum(posA1s) >0
-        maxIdx              = find(detected.LLRatio==max(detected.LLRatio(currStats.PixelIdxList(posA1s))),1);
-        maxIdxInPixelList   = find(maxIdx==currStats.PixelIdxList);
-        A1est  = detected.A1(maxIdx);
-        B1est  = detected.B1(maxIdx);
-        B0est  = detected.B0(maxIdx);
-        XYZest = {cellfun(@(x) x(maxIdxInPixelList),currDomains)};
-        theta0 = num2cell([XYZest{:};gaussSigmas(:);A1est;B1est]);
+    A1s          = currA1sBasket{i};
+    posA1s = A1s > 0;
+    if any(A1s)
+        [~,maxLLRatioIdx] = max(currLLRBasket{i}.*posA1s);
+        A1est           = A1s(maxLLRatioIdx);
+        B1est           = currB1sBasket{i}(maxLLRatioIdx);
+        B0est           = currB0sBasket{i}(maxLLRatioIdx);
+        XYZest          = cellfun(@(x) x(maxLLRatioIdx),currDomainBasket{i});
+        theta0          = num2cell([XYZest' gaussSigmas A1est B1est]);
         % iterative MLE
-        spotParams{i} = MLEbyIteration(currData,theta0,currReadNoise,currDomains,params);
+        spotParams{i}   = MLEbyIteration(currData,theta0,currReadNoise,currDomains,params);
     end
     incrementParForProgress();
 end
