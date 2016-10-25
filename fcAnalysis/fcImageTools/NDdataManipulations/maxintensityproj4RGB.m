ac4689e2501e393384d0f0d1eca312f02e00ecf8
fred chang <fchang@fas.harvard.edu>
2016-10-24 21:41:03 -0400

    rgb max intensity projection is up

--- fcAnalysis/fcImageTools/NDdataManipulations/maxintensityproj4RGB.m
+++ fcAnalysis/fcImageTools/NDdataManipulations/maxintensityproj4RGB.m
@@ -1,14 +1,34 @@
-function [ output_args ] = maxintensityproj4RGB( input_args )
-%MAXINTENSITYPROJ4RGB Summary of this function goes here
-%   Detailed explanation goes here
+function maxRGBmarked = maxintensityproj4RGB(rgbDataWmarkers,direction)
+%MAXINTENSITYPROJ4RGB when you do a max projection of a bw rgb image with
+%colored markers, the max projection needs to be handled carefully.  the
+%image is 2d.
+sizeData = size(rgbDataWmarkers);
+colors = {[1 0 0]',[0 1 0]',[0 0 1]'};
+rgbVals = reshape(permute(rgbDataWmarkers,[3 1 2]),3,numel(rgbDataWmarkers)/3);
+maxVal = double(max(rgbDataWmarkers(:)));
+diffBasket = cell(numel(colors),1);
+for ii = 1:numel(colors);
+    currDiff = bsxfun(@minus,double(rgbVals),colors{ii}*maxVal);
+    diffBasket{ii} = find(cellfun(@norm,num2cell(currDiff,1))==0);
+end
 
-[maxgray, maxidx] = max(grayStack, [], 3);  %along the 3rd axis (Z)
-nX = size(Stack,1);
-nY = size(Stack,2);
-[X, Y] = ndgrid( 1 : nX, 1 : nY );
-MIP_R = Stack( sub2idx( size(Stack), X(:), Y(:), 1, maxidx(:) ) );
-MIP_G = Stack( sub2idx( size(Stack), X(:), Y(:), 2, maxidx(:) ) );
-MIP_B = Stack( sub2idx( size(Stack), X(:), Y(:), 3, maxidx(:) ) );
-MIP = cat(3, reshape(MIP_R, nX, nY), reshape(MIP_G, nX, nY), reshape(MIP_B, nX, nY) );
-image(MIP);
-
+maxRGBmarked = rgbDataWmarkers(:,:,1);
+maxRGBmarked = max(maxRGBmarked,[],direction);
+maxRGBmarked = bw2rgb(maxRGBmarked);
+for ii = 1:numel(colors)
+    [xd,yd] = ind2sub(sizeData(1:2),diffBasket{ii});
+    if ~isempty(xd) || ~isempty(yd)
+        switch direction
+            case 1
+                for jj = 1:numel(yd)
+                    maxRGBmarked(1,yd(jj),:) = colors{ii}*maxVal;
+                end
+            case 2
+                for jj = 1:numel(xd)
+                    maxRGBmarked(xd(jj),1,:) = colors{ii}*maxVal;
+                end
+            otherwise
+                error('did not program for that direction');
+        end
+    end
+end
