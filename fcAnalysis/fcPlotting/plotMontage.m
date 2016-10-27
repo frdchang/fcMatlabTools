7f9d6f693bcfaa3235531544e5d5a457dded2695
Frederick Chang <fchang@fas.harvard.edu>
2016-10-27 10:41:22 -0400

    added phase images to kymo

--- fcAnalysis/fcPlotting/plotMontage.m
+++ fcAnalysis/fcPlotting/plotMontage.m
@@ -1,8 +1,8 @@
-function h = plotMontage(SubV)
+function h = plotMontage(SubV,varargin)
 %PLOTMONTAGE Summary of this function goes here
 %   Detailed explanation goes here
 
-h = montage(reshape(SubV,[size(SubV,1), size(SubV,2), 1, size(SubV,3)]),'DisplayRange',[]);
+h = montage(reshape(SubV,[size(SubV,1), size(SubV,2), 1, size(SubV,3)]),'DisplayRange',[],varargin{:});
 
 end
 
