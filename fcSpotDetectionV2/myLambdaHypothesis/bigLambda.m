d6ce48a16de594e0ba107eba920022be700e49bb
Frederick Chang <fchang@fas.harvard.edu>
2016-12-21 19:27:10 -0500

    starting on bigLambda

--- fcSpotDetectionV2/myLambdaHypothesis/bigLambda.m
+++ fcSpotDetectionV2/myLambdaHypothesis/bigLambda.m
@@ -1,22 +1,32 @@
-function [bigLambda,bigDLambda,bigD2Lambda] = bigLambda(Kmatrix,domains,thetaInputs,maxThetasInputs,varargin)
+function [bigLambdas,bigDLambdas,bigD2Lambdas] = bigLambda(Kmatrix,domains,thetaInputs,maxThetasInputs,varargin)
 %BIGLAMBDA generates the lambda hypothesis for multi-spectral datasets with
 %spectral bleed thru modeled by the Kmatrix = bleedthru matrix.  the
 %diagonal entries are 1. 
 
-% thetaInputs = {Kmatrix,{patternObj1,theta1},{patternObj2,theta2},B}
-
+% thetaInputs = {thetaInputForChannel1,thetaInputForChannel2,...}
+% with thetaInputForChannel1 e.g. = {{patternObj1,theta1},{patternObj2,theta2},B}
+% 
+% maxThetasInputs are similar
+% 
+% varargin passes directly into littleLambda
+%
+% Kmatrix is the bleed thru matrix, with the rows the bleed thru.  the
+% diagonal elements are assumed to be 1.
+%
+% the gradients and hessian are independent 
 
 numDatas = size(Kmatrix,2);
+littleLambdas = cell(numDatas,1);
+littleDLambdas = cell(numDatas,1);
+littleD2Lambdas = cell(numDatas,1);
+% generate little lambdas for each channel
+for ii = 1:numDatas
+    [littleLambdas{ii},littleDLambdas{ii},littleD2Lambdas{ii}] = littleLambda(domains,thetaInputs{ii},maxThetasInputs{:},varargin{:});
+end
 
-[littleLambda,littleDLambda,littleD2Lambda] = littleLambda(domains,thetaInputs,maxThetasInputs,varargin{:});
-
-bigLambda   = cell(numDatas,1);
-bigDLambda  = cell(numDatas,1);
-bigD2Lambda = cell(numDatas,1);
-
-[bigLambda{:}] = applyKmatrix(Kmatrix,littleLambda);
-[bigDLambda{:}] = applyKmatrix(Kmatrix,littleDLambda);
-[bigD2Lambda{:}] = applyKmatrix(Kmatrix,littleD2Lambda,maxThetasInputs);
+bigLambdas   = applyKmatrix(Kmatrix,littleLambdas);
+bigDLambdas  = applyKmatrix(Kmatrix,littleDLambdas);
+bigD2Lambdas = applyKmatrix(Kmatrix,littleD2Lambdas,maxThetasInputs);
 
 end
 
--- fcSpotDetectionV2/myLambdaHypothesis/helper/applyKmatrix.m
+++ fcSpotDetectionV2/myLambdaHypothesis/helper/applyKmatrix.m
@@ -3,21 +3,5 @@ function varargout = applyKmatrix(Kmatrix,littleLambdas,varargin)
 %lambdas
 
 numDatas = size(Kmatrix,2);
-
 varargout = cell(numDatas,1);
-
-if isempty(varargin)
-   if iscell(littleLambdas)
-       
-   else
-      for ii = 1:numDatas
-         varargout{ii} = Kmatrix(ii) 
-      end
-   end
-else
-    
-end
-
-
-end
 
