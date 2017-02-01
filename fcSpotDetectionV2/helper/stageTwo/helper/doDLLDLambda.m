5ce22a1658703e056cb25e593560d96f9f0621ad
Frederick Chang <fchang@fas.harvard.edu>
2017-02-01 10:50:34 -0500

    fixed newton bug...git add --all yay

--- fcSpotDetectionV2/helper/stageTwo/helper/doDLLDLambda.m
+++ fcSpotDetectionV2/helper/stageTwo/helper/doDLLDLambda.m
@@ -7,7 +7,7 @@ D2LLDLambdas2 = cell(size(datas));
 
 for ii = 1:numel(datas)
    DLLDLambdas{ii} =  DLLDLambdaFunc(datas{ii},bigLambdas{ii},sigmasqs{ii},1);
-   D2LLDLambdas2{ii} =  DLLDLambdaFunc(datas{ii},bigLambdas{ii},sigmasqs{ii},1);
+   D2LLDLambdas2{ii} =  DLLDLambdaFunc(datas{ii},bigLambdas{ii},sigmasqs{ii},2);
 end
 
 
