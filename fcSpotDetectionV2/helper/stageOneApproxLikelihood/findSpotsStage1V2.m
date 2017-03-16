d798765ddb312d5b99e02fc320e4f4c57f90c161
Frederick Chang <fchang@fas.harvard.edu>
2017-03-16 11:04:55 -0400

    added spot kern cross terms in calculation to prep for LLRatio

--- fcSpotDetectionV2/helper/stageOneApproxLikelihood/findSpotsStage1V2.m
+++ fcSpotDetectionV2/helper/stageOneApproxLikelihood/findSpotsStage1V2.m
@@ -224,6 +224,17 @@ else
     % be similar and when kernels are equal, this is the true answer.  this
     % is an approximation.  note that i will write an arrayfun version that
     % will simply calculate directly.
+    
+    % calcualte cross terms for spotKern
+    numKerns = numel(spotKern);
+    numCrossTerms = ((numKerns-1)^2) + (numKerns-1)/2;
+    crossSpotKerns = cell(numCrossTerms,1);
+    myIndex = 1;
+    for ii = 1:numel(spotKern)
+        for jj = ii+1:numel(spotKern)
+            crossSpotKerns{myIndex} = convFunc(invVarSaved,spotKern{ii}.*spotKern{jj});
+        end 
+    end
     squaredCompLL1 = calcModelSquaredForLL1(kMatrix,A1,B1,sumCellContents(k1)/numel(spotKern),sumCellContents(k3)/numel(spotKern),k5);
     crossCompLL1   = calcModelCrossForLL1(kMatrix,A1,B1,k2,k4);
     LL1            = -(squaredCompLL1 + crossCompLL1);
