e62b43af154ea9cafd22e8a68afcf651d2b1e215
fred chang <fchang@fas.harvard.edu>
2017-04-22 11:38:48 -0400

    cleaned up binning to handle 1D and on better

--- fcSpotDetectionV2/helper/stageTwo/helper/myPattern/helper/NDbinData.m
+++ fcSpotDetectionV2/helper/stageTwo/helper/myPattern/helper/NDbinData.m
@@ -11,32 +11,34 @@ function [binned] = NDbinData(data,patchSize)
 % test = test(5:5:end,5:5:end,5:5:end);
 % isequal(test,out)  9/23/16 -fc
 
-numDims = ndims(data);
-if numDims == 2
-   if size(data,1) == 1 || size(data,2) ==1
-      numDims =1; 
-   end
+% if no binning, patchSize is a ones vector or isempty just return data
+if all(patchSize==1) || isempty(patchSize)
+   binned = data;
+   return;
 end
 
-if isscalar(patchSize) && numDims ==1
-    binned = sum(reshape(data,patchSize,[]),1)/patchSize;
-    return;
+% if data is a vector, pathSize needs to have 1 binning for the 1 dimension
+% to make it match in size
+
+if isvector(data)
+   temp = ones(1,numel(size(data)));
+   temp(size(data)~=1) = patchSize;
+   patchSize = temp;
 end
-if ~isequal(numDims,numel(patchSize))
-    binned = data;
-    return;
+
+% otherwise make sure data is integer increments of patchSize.  if not pad
+% by zeros at the end
+
+increments = size(data)./patchSize;
+
+% if increments not integer, pad by zeros
+if ~isequal(round(increments),increments)
+    data = padarray(data, round((ceil(increments)-increments).*patchSize),0,'post');
 end
 outputSize = size(data)./patchSize;
-if isequal(outputSize,size(data))
-    binned = data;
-    return;
-end
-if ~isequal(round(outputSize),outputSize)
-    error('patchSize is not a clean multiple of the dimensions of data');
-end
 
 temp = sum(reshape(data,patchSize(1),[]),1)/patchSize(1);
-for i = 2:numDims   
+for i = 2:ndims(data)   
     temp = sum(reshape(temp,prod(outputSize(1:i-1)),patchSize(i),[]),2)/patchSize(i);
 end
 binned = reshape(temp,outputSize);
