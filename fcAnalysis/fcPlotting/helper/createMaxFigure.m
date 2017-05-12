d0c02075d8b6ff94b940f19912fd021e23838daa
fred chang <fchang@fas.harvard.edu>
2017-05-11 20:04:21 -0400

    analysis stage II is rudimentary up

--- fcAnalysis/fcPlotting/helper/createMaxFigure.m
+++ fcAnalysis/fcPlotting/helper/createMaxFigure.m
@@ -1,14 +1,19 @@
-function h = createMaxFigure()
+function h = createMaxFigure(varargin)
 %CREATEMAXFIGURE creates a maximum figure window given the users monitor
 %resolution
 %
 % fchang@fas.harvard.edu
 shrinkFactor = 1;
-set(0,'units','pixels');  
+set(0,'units','pixels');
 BBoxScreen = get(0,'screensize');
 width = BBoxScreen(3);
 height = BBoxScreen(4);
-h = figure('Position',round([height*(1-shrinkFactor),height*(1-shrinkFactor),height*shrinkFactor,height*shrinkFactor]));
 
+if isempty(varargin)
+    h = figure('Position',round([height*(1-shrinkFactor),height*(1-shrinkFactor),height*shrinkFactor,height*shrinkFactor]));
+else
+    h = figure('Position',round([height*(1-shrinkFactor),height*(1-shrinkFactor),height*shrinkFactor,height*shrinkFactor]),'Name',varargin{1},'NumberTitle','off');
+    
+end
 end
 
--- fcSpotDetectionV2/testFunctions/benchmark/analyzeStageII.m
+++ fcSpotDetectionV2/testFunctions/benchmark/analyzeStageII.m
@@ -36,6 +36,9 @@ for ii = 1:numConds
         analysis{ii}.thetaHolder = thetaHolder;
         analysis{ii}.LLPPHolder  = LLPPHolder;
         analysis{ii}.trueTheta   = trueTheta;
+        analysis{ii}.A           = currA;
+        analysis{ii}.B           = currB;
+        analysis{ii}.D           = currD;
     end
 end
 
@@ -43,7 +46,15 @@ end
 % iterate over distances, numspots, conditions
 numTheta      =  getFirstNonEmptyCellContent(analysis);
 numTheta      =  size(numTheta.thetaHolder,2);
-sizeConditions = size(stageIIconds);
+
+
+
+% gen histogram maps
+histograms = cell(numTheta,1);
+for ii = 1:numTheta
+    histograms{ii} = genHist(analysis,ii);
+end
+
 
 % get std map
 stdForEachTheta = cell(numTheta,1);
@@ -51,10 +62,41 @@ for ii = 1:numTheta
     [stdForEachTheta{ii},domains] = applyFunc(stageIIconds,analysis,@std,ii);
 end
 
+for ii = 1:numTheta
+    figure;
+    currTheta = ii;
+    d = 1;
+    contour(domains{1}(:,:,d),domains{2}(:,:,d),stdForEachTheta{currTheta}(:,:,d));
+    xlabel('A');ylabel('B');title(['std deviation theta ' num2str(ii)]);
+end
+
 % get mean map
 meanForEachTheta = cell(numTheta,1);
 for ii = 1:numTheta
     [meanForEachTheta{ii},~] = applyFunc(stageIIconds,analysis,@mean,ii);
+end
+
+
+
+end
+
+function hBasket = genHist(analysis,currTheta)
+sizeConditions = size(analysis);
+for di = 1:sizeConditions(3)
+    currAnalysis = analysis(:,:,di);
+    currDFirst = getFirstNonEmptyCellContent(currAnalysis);
+    currD = currDFirst.D;
+    hBasket{di} = createMaxFigure(['distance:' num2str(currD) ' theta(' num2str(currTheta) ') of ' num2str(currDFirst.trueTheta(:)')]);
+    
+    currSizeConditions = size(currAnalysis);
+    for ii = 1:prod(currSizeConditions)
+        if ~isempty(currAnalysis{ii})
+            subplot(currSizeConditions(1), currSizeConditions(2),ii);
+            hSub = histogram(currAnalysis{ii}.thetaHolder(currTheta,:));
+            hSub.Normalization = 'pdf';
+            title(['A:' num2str(currAnalysis{ii}.A) ' B:' num2str(currAnalysis{ii}.B)]);
+        end
+    end
 end
 end
 
