8c3a082654b500ec4ee28abe66a4b9cfe263e6b6
fred chang <fchang@fas.harvard.edu>
2017-05-16 19:45:43 -0400

    gamma fit

--- fcSpotDetectionV2/testFunctions/benchmark/analyzeStageI.m
+++ fcSpotDetectionV2/testFunctions/benchmark/analyzeStageI.m
@@ -12,7 +12,7 @@ idxConditions = zeros(size(conditions));
 idxConditions(:,:,1) = 1;
 conditions = conditions(logical(idxConditions));
 
-h = createMaxFigure(conditionFunc);
+
 conditionHolder = cell(size(conditions));
 currMin = inf;
 currMax = -inf;
@@ -57,7 +57,7 @@ for ii = 1:prod(sizeAB)
     end
 end
 
-
+h = createMaxFigure([conditionFunc ' cdf']);
 for ii = 1:prod(sizeAB)
     currA = conditions{ii}.A;
     currB = conditions{ii}.B;
@@ -75,10 +75,61 @@ for ii = 1:prod(sizeAB)
 end
 legend('bk','sig');
 
+
 for ii = 1:prod(sizeAB)
     hold on;subplot(sizeAB(1),sizeAB(2),ii);
     axis([myMin 10^ceil(log10(currMax)) 0 1]);
     set(gca,'XScale','log');
 end
 
+
+
+
+
+%% fit gamma distribution
+h = createMaxFigure([conditionFunc ' pdf background and gamma fit']);
+bkShape = zeros(sizeAB);
+bkScale = zeros(sizeAB);
+for ii = 1:prod(sizeAB)
+    currA = conditions{ii}.A;
+    currB = conditions{ii}.B;
+%     sig = conditionHolder{ii}.sig + currMin + myMin;
+    bk = conditionHolder{ii}.bk;
+    subplot(sizeAB(1),sizeAB(2),ii);
+    hBk  = histogram(bk);
+    phat = gamfit(bk);
+    hBk.Normalization = 'pdf';
+    hold on;
+    plot(hBk.BinEdges,gampdf(hBk.BinEdges,phat(1),phat(2)));
+    hBk.EdgeColor = 'none';
+    title({['A' num2str(currA) ' B' num2str(currB)],[num2str(phat(1)) ',' num2str(phat(2))]});
+    axis tight;
+    bkShape(ii) = phat(1);
+    bkScale(ii) = phat(2);
+end
+figure;imagesc(bkShape');colorbar;title('bk shape');
+figure;imagesc(bkScale');colorbar;title('bk scale');
+
+%% fit gamma distribution
+h = createMaxFigure([conditionFunc ' pdf signal ']);
+sigShape = zeros(sizeAB);
+sigScale = zeros(sizeAB);
+for ii = 1:prod(sizeAB)
+    currA = conditions{ii}.A;
+    currB = conditions{ii}.B;
+     sig = conditionHolder{ii}.sig;
+    subplot(sizeAB(1),sizeAB(2),ii);
+    hBk  = histogram(sig);
+    phat = gamfit(sig);
+    hBk.Normalization = 'pdf';
+    hold on;
+    plot(hBk.BinEdges,gampdf(hBk.BinEdges,phat(1),phat(2)));
+    hBk.EdgeColor = 'none';
+ title({['A' num2str(currA) ' B' num2str(currB)],[num2str(phat(1)) ',' num2str(phat(2))]});
+    axis tight;
+    sigShape(ii) = phat(1);
+    sigScale(ii) = phat(2);
+end
+figure;imagesc(sigShape');colorbar;title('sig shape');
+figure;imagesc(sigScale');colorbar;title('sig scale');
 
