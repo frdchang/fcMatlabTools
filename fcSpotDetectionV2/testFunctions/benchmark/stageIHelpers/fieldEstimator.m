a2b002d71aebb48b956e79ed39f774fd7879f5b1
fred chang <fchang@fas.harvard.edu>
2017-05-21 11:29:57 -0400

    added gamma significance testing for LLRatio

---
+++ fcSpotDetectionV2/testFunctions/benchmark/stageIHelpers/fieldEstimator.m
@@ -0,0 +1,35 @@
+function [ estimated ] = fieldEstimator(data,spotKern,cameraVariance,varargin)
+%FIELDESTIMATOR will use gradient fields and hessian fields
+% assums there is only 1 psf
+
+if iscell(data)
+data = data{1};
+spotKern = spotKern{1};
+end
+
+estimated = findSpotsStage1V2(data,spotKern,cameraVariance,varargin{:});
+[gradientFieldFilters,hessFieldFilters,kerns] = genFieldFilters(size(spotKern),spotKern);
+
+
+gradOfData = calcFullGradientFilter(data,estimated,kerns.kern,kerns.kernDs,cameraVariance);
+[gradXYZDotProduct] = convFieldKernels(gradOfData(3:end),gradientFieldFilters(3:end));
+
+hessOfData = calcFullHessianFilter(data,estimated,kerns.kern,kerns.kernDs,kerns.kernD2s,cameraVariance);
+[hessXYZDotProduct] = convFieldKernels(hessOfData(3:end,3:end),hessFieldFilters(3:end,3:end));
+
+gradXYZDotProduct(gradXYZDotProduct<0) = 0;
+hessXYZDotProduct(hessXYZDotProduct<0) = 0;
+
+gradXYZDotProduct = norm0to1(gradXYZDotProduct);
+hessXYZDotProduct = norm0to1(hessXYZDotProduct);
+
+
+gradDOTLLRatio = gradXYZDotProduct.*estimated.LLRatio;
+hessDOTLLRatio = hessXYZDotProduct.*estimated.LLRatio;
+
+gradHessDOTLLRatio = gradXYZDotProduct.*hessXYZDotProduct.*estimated.LLRatio;
+
+estimated.gradDOTLLRatio = gradDOTLLRatio;
+estimated.hessDOTLLRatio = hessDOTLLRatio;
+estimated.gradHessDOTLLRatio = gradHessDOTLLRatio;
+
---
+++ fcSpotDetectionV2/testFunctions/benchmark/stageIHelpers/gammaCorrection.m
@@ -0,0 +1,15 @@
+function [ estimated ] = gammaCorrection( data,spotKern,cameraVariance,varargin )
+%GAMMACORRECTION appens the gamma pdf correction.  only applies to single
+%channel data
+if iscell(data)
+   data = data{1};
+   spotKern = spotKern{1};
+end
+[estimated] = findSpotsStage1V2(data,spotKern,cameraVariance,varargin{:});
+
+gammaA = 0.5;
+gammaB = @(b) 2.6*b + 2.1;
+
+gamsig = arrayfun(@(llr,b) 1-gamcdf(llr,gammaA,gammaB(b)),estimated.LLRatio,estimated.B1);
+estimated.gammaSig = gamsig;
+
