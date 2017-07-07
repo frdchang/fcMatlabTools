de7b7f3873792474458f519ffed5d7034274f354
fred chang <fchang@fas.harvard.edu>
2017-07-07 10:42:40 -0400

    colored z rpoject

---
+++ fcAnalysis/fcPlotting/helper/maxColoredProj.m
@@ -0,0 +1,14 @@
+function [ coloredProj ] = maxColoredProj(data,dim)
+%MAXCOLOREDPROJ creates and rgb colormapped in the dimension specified
+myCmapFunc = @parula;
+
+data = norm0to1(data);
+[maxproj,idx] = max(data,[],dim);
+
+zcolormap = ind2rgb(idx,myCmapFunc(size(data,dim)));
+hsvColormap = rgb2hsv(zcolormap);
+hsvColormap(:,:,3) = maxproj;
+coloredProj = hsv2rgb(hsvColormap);
+coloredProj = uint8(255*coloredProj);
+end
+
---
+++ fcAnalysis/fcProcessing/modules/procProjectStageI.m
@@ -0,0 +1,18 @@
+function [projFuncOutput] = procProjectStageI(stageIOutput,varargin )
+%PROCAPPLYTOIMAGES Summary of this function goes here
+%   Detailed explanation goes here
+%--parameters--------------------------------------------------------------
+params.projFunc     = @maxColoredProj;
+params.projFuncArg  = {3};
+%--------------------------------------------------------------------------
+params = updateParams(params,varargin);
+
+
+allFiles = flattenCellArray(stageIOutput.outputFiles);
+imageFiles = keepCertainStringsUnion(allFiles,{'tif','fits'});
+imageFiles = convertListToListofArguments(imageFiles);
+
+projFuncOutput    = applyFuncTo_listOfListOfArguments(imageFiles,@openImage_applyFuncTo,{},params.projFunc,{params.projFuncArg{:}},@saveToProcessed_images,{},'doParallel',false);
+
+end
+
