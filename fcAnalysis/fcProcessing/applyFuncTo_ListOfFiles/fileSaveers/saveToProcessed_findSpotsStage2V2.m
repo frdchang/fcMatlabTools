d7a6f71928cd26297cbb0644a8c382d1baa42683
fred chang <fchang@fas.harvard.edu>
2017-07-19 12:13:46 -0400

    proc stage ii up

---
+++ fcAnalysis/fcProcessing/applyFuncTo_ListOfFiles/fileSaveers/saveToProcessed_findSpotsStage2V2.m
@@ -0,0 +1,13 @@
+function [ outputs ] = saveToProcessed_findSpotsStage2V2(listOfFileInputPaths,funcOutput,myFunc,funcParamHash,varargin)
+%SAVETOPROCESSED_FINDSPOTSSTAGE2V2 Summary of this function goes here
+%   Detailed explanation goes here
+
+fileInputs = listOfFileInputPaths{1};
+saveProcessedFileAt = genProcessedFileName(fileInputs,myFunc,'paramHash',funcParamHash);
+
+stageIIMLEs = funcOutput{1};
+makeDIRforFilename(saveProcessedFileAt);
+save(saveProcessedFileAt,'stageIIMLEs');
+outputs = table({[saveProcessedFileAt '.mat']},{stageIIMLEs},'VariableNames',{'pathToStageIIMLEs','stageIIMLEs'});
+
+
