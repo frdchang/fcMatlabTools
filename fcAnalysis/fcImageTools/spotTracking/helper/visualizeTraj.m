366aba68f5db28e14c1cf99819b1398de665367a
fred chang <fchang@fas.harvard.edu>
2017-08-09 10:47:01 -0400

    visualizing tracker

--- fcAnalysis/fcImageTools/spotTracking/helper/visualizeTraj.m
+++ fcAnalysis/fcImageTools/spotTracking/helper/visualizeTraj.m
@@ -1,23 +1,15 @@
-function [  ] = visualizeTraj( tracked )
+function [h] = visualizeTraj( tracked )
 %VISUALIZETRAJ Summary of this function goes here
 %   Detailed explanation goes here
-nframe = numel(peaks);
-% C = peaks{1}(:,1);
-% R = peaks{1}(:,2);
-% X = [[C'-2; C'+2], [C'; C']];
-% Y = [[R'; R'], [R'-2; R'+2]];
-% hand = line(X,Y);
-% set(hand(:),'Color',[1 0 0]);
-for iframe=2:nframe
-    oldind = find(peaks{iframe-1}(:,6)>0);
-    curind = peaks{iframe-1}(oldind,6);
-    X = [peaks{iframe-1}(oldind,1), peaks{iframe}(curind,1)];
-    Y = [peaks{iframe-1}(oldind,2), peaks{iframe}(curind,2)];
-    hand = line(X',Y');
-    set(hand(:),'LineWidth',[1.0]);
-end
-hold off
-
+numTracks = numel(tracked);
+myMap     = distinguishable_colors(numTracks,[0 0 0]);
+hold on;
+for ii=1:numTracks
+   currTrack =  tracked{ii};
+   X = currTrack(:,2);
+   Y = currTrack(:,3);
+   Z = currTrack(:,4);
+   line(X,Y,Z,'Marker','o','Color',myMap(ii,:));
 
 end
 
