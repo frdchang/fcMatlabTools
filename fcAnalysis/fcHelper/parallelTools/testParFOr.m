a81f2d25d57b3f0744d54fa23827c08e1e627de5
fred chang <fchang@fas.harvard.edu>
2017-08-22 11:01:04 -0400

    try this

--- fcAnalysis/fcHelper/parallelTools/testParFOr.m
+++ fcAnalysis/fcHelper/parallelTools/testParFOr.m
@@ -1,12 +1,13 @@
-function [ output ] = testParFOr()
+function [ output ] = testParFOr(N)
 %TESTPARFOR Summary of this function goes here
 %   Detailed explanation goes here
 
-N = 2;
 output = cell(N,1);
 setupParForProgress(N);
 parfor ii = 1:N
-    output{ii} = bench;
+    test = rand(1048,1048,11);
+    tic;test = fft(test);
+    output{ii} = toc;
     incrementParForProgress();
 end
 
