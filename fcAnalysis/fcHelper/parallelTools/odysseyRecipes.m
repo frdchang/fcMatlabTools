a41d42ce2725843cc44d49bd45f642e7e6255575
Fred Chang <frdchang@gmail.com>
2017-08-26 12:15:47 -0400

    asdf

--- fcAnalysis/fcHelper/parallelTools/odysseyRecipes.m
+++ fcAnalysis/fcHelper/parallelTools/odysseyRecipes.m
@@ -1,9 +1,9 @@
 %% see if i can max out the workers
-odysseyConfig();
-
-numBatches = 700;
+setupCluster('setWallTime','00:10:00','setMemUsage','4000');
+numBatches = 10;
 c = parcluster;
 j = cell(numBatches,1);
 for ii = 1:numBatches
    j{ii} = c.batch(@testParFOr, 1, {256}, 'pool', 12); 
 end
+
