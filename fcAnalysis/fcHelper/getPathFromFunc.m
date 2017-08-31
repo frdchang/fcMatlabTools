d1d740040f6ac7faca35f56d81c70395452c67ae
fred chang <fchang@fas.harvard.edu>
2017-08-31 10:16:01 -0400

    try

--- fcAnalysis/fcHelper/getPathFromFunc.m
+++ fcAnalysis/fcHelper/getPathFromFunc.m
@@ -1,11 +1,14 @@
 function [ myPath ] = getPathFromFunc()
 %GETPATHFROMFUNC Summary of this function goes here
 %   Detailed explanation goes here
-p = mfilename('fullpath');
-[path,filename,ext] = fileparts(p);
-path = regexp(path,'(?:(?!fcAnalysis).)*','match');
-path = path{1};
-myPath = {[path 'fcAnalysis'],[path 'fcSpotDetectionV2']};
+% p = mfilename('fullpath');
+% [path,filename,ext] = fileparts(p);
+% path = regexp(path,'(?:(?!fcAnalysis).)*','match');
+% path = path{1};
+% myPath = {[path 'fcAnalysis'],[path 'fcSpotDetectionV2']};
 
+myPath = path;
+myPath = strsplit(myPath,':');
+myPath = keepCertainStringsUnion(myPath,'fcMatlabTools');
 end
 
