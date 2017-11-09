96ccbc5a2692ac6e1521e8b4140b7042c5d3e270
fred chang <fchang@fas.harvard.edu>
2017-11-09 15:18:35 -0500

    eer explanation

--- fcAnalysis/fcRecipes/forFred/spotDetectionPaper/FDRTutorial.m
+++ fcAnalysis/fcRecipes/forFred/spotDetectionPaper/FDRTutorial.m
@@ -15,3 +15,37 @@ benchStruct = procBenchMarkStageI(benchStruct,@findSpotsStage1V2);
 analyzeStageIFPRPlot(benchStruct,@regularConv,'regularConv');
 % analyzeStageIFPR(benchStruct,@gammaCorrection,'negLoggammaSig');
 %% this is the final version
+close all;
+sig = 0.05;
+x = 0:0.1:10;
+bA = 1;
+bB = 2;
+sA = 2;
+sB = 2;
+bkgnd = gampdf(x,bA,bB);
+signal = gampdf(x,sA,sB);
+cumBkgnd = gamcdf(x,bA,bB);
+cumSig   = gamcdf(x,sA,sB);
+idx = find(cumBkgnd > (1-sig),1);
+eer = intersections(x,1-cumBkgnd,x,cumSig);
+
+subplot_tight(4,1,1);
+plot(x,bkgnd);hold on;plot(x,signal);axis tight;
+vline(x(idx),'g',num2str(sig));
+vline(eer,'r','EER');
+
+
+subplot_tight(4,1,2);
+plot(x,cumBkgnd); hold on; plot(x,cumSig); 
+vline(x(idx),'g',num2str(sig));
+vline(eer,'r','EER');
+
+subplot_tight(4,1,3);
+plot(x,1-cumBkgnd);hold on; plot(x,cumSig);legend('fpr','fnr');
+vline(x(idx),'g',num2str(sig));
+vline(eer,'r','EER');
+
+subplot_tight(4,1,4);
+plot(x,cumBkgnd);hold on; plot(x,1-cumSig);legend('tnr (selectivity)','tpr(sensitivity)');
+vline(x(idx),'g',num2str(sig));
+vline(eer,'r','EER');
