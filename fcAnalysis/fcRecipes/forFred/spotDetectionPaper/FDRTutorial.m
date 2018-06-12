8c77a7065b7ce9a33ba59c1f6b83645f3a8bef76
fred chang <fchang@fas.harvard.edu>
2018-06-12 11:11:40 -0400

    background shifting demo

--- fcAnalysis/fcRecipes/forFred/spotDetectionPaper/FDRTutorial.m
+++ fcAnalysis/fcRecipes/forFred/spotDetectionPaper/FDRTutorial.m
@@ -49,3 +49,29 @@ subplot_tight(4,1,4);
 plot(x,cumBkgnd);hold on; plot(x,1-cumSig);legend('tnr (selectivity)','tpr(sensitivity)');
 vline(x(idx),'g',num2str(sig));
 vline(eer,'r','EER');
+
+%% generate different backgrounds to show background robustness
+%% this is the final version
+close all;
+sig = 0.05;
+x = 0:0.1:10;
+bA = 1;
+bB = 2;
+sA = 2;
+sB = 2;
+bkgnd = gampdf(x,bA,bB);
+signal = gampdf(x,sA,sB);
+cumBkgnd = gamcdf(x,bA,bB);
+cumSig   = gamcdf(x,sA,sB);
+idx = find(cumBkgnd > (1-sig),1);
+eer = intersections(x,1-cumBkgnd,x,cumSig);
+
+subplot_tight(4,1,1);
+plot(x,bkgnd);hold on;plot(x,signal);axis tight;
+thisAxis = axis;
+subplot_tight(4,1,2);
+plot(x,gampdf(x,0.5,bB));hold on;plot(x,signal);axis tight;
+axis(thisAxis);
+subplot_tight(4,1,3);
+plot(x,gampdf(x,0.25,bB));hold on;plot(x,signal);axis tight;
+axis(thisAxis);
